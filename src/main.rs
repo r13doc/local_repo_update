badd1d2a7041c1327de5f2758c97f89fda0de2fe
r13doc <r13doc@yandex.ru>
2025-05-23 15:55:32 +0300

    some updates

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::io::{BufRead, Write};
 use std::path::{Path, PathBuf};
 use tokio::process::Command;
 use std::str::from_utf8;
+use chrono::Local;
+
+
 
 async fn write_logs(data: &[u8]) {
     let p = current_dir()
@@ -22,11 +25,12 @@ async fn write_logs(data: &[u8]) {
 
 #[tokio::main]
 async fn main() {
+    
     let log_file = current_dir()
         .unwrap()
         .as_path()
         .join("update_repo.log");
-
+    
     if !log_file.exists() {
         fs::File::create(&log_file)
             .expect("something wrong with creating log_file");
@@ -36,29 +40,30 @@ async fn main() {
         .open(log_file.as_path())
         .await
         .expect("prob with clear log"); }
-
+    
     // write data to log
-    let time = chrono::Utc::now();
+    let time = chrono::Utc::now().with_timezone(&Local);
     let mut time = time.format("%Y-%m-%d %H:%M:%S").to_string();
     time.push('\n');
     let d = time.as_bytes();
     write_logs(d).await;
     
     // path for works with 
-    let path = current_dir().unwrap();
-
+    //let path = current_dir().unwrap();
+    let path = Path::new("/home/den/venv/STUDY/").to_path_buf();
+    
     let main_path = read_dir(path)
         .expect("missing directory")
         .map(|res|res.map(|f| f.path()))
         .collect::<Vec<_>>();
-
+    
     let mut nested_dirs = vec![];
     for i in main_path {
         let res = match &i {
             Ok(i) if !i.is_dir() => {continue},
             _ => {i.unwrap()}
         };
-
+    
         for dir in read_dir(res).unwrap() {
             nested_dirs.push(dir.unwrap().path())
         }
@@ -70,7 +75,7 @@ async fn main() {
         .output()
         .await
         .expect("error git fetch");
-
+    
     let git_stash = async move |dir: PathBuf| Command::new("git")
         .current_dir(dir)
         .args(["stash"])
@@ -96,36 +101,40 @@ async fn main() {
             } else {
                 // pull request to update
                 let pull = git_pull(i.clone()).await;
-
+    
                 //tokio::time::sleep(Duration::from_secs(10)).await;
                 let da = pull.stdout.as_slice();
-
+    
                 let result = from_utf8(da)
                     .unwrap()
                     .lines()
                     .find(|l| l.contains("Получение обьектов")
                                 | l.contains("Уже актуально")
                                 | l.contains("error"));
-
+    
                 match result {
                     Some(a) if a.contains("Уже актуально") => {
                         let data = result.unwrap().as_bytes();
                         write_logs([data, b" -> ",out.stdout.as_slice()].concat().as_slice()).await;
                     }
-
+                    Some(a) if a.contains("Получение обьектов") => {
+                        let data = result.unwrap().as_bytes();
+                        write_logs([data, b" -> ",out.stdout.as_slice()].concat().as_slice()).await;
+                    }
+    
                     None => {
                         // if problems with update repo run stash - pull
                         git_stash(i.clone()).await;
                         let pull  = git_pull(i.clone()).await;
                         let da = pull.stdout.as_slice();
-
+    
                         let result = from_utf8(da)
                             .unwrap()
                             .lines()
                             .find(|l| l.contains("Получение обьектов")
                                 | l.contains("Уже актуально")
                                 | l.contains("error") | l.contains(""));
-
+    
                         // problem with update local repo error will send to log file
                         if result.is_none() {
                             let res = pull.stderr.as_slice();
@@ -133,11 +142,11 @@ async fn main() {
                                 .lines()
                                 .find(|l| l.as_ref().unwrap().contains("."))
                                 .unwrap();
-
+    
                             let data = result.unwrap();
                             write_logs([data.as_bytes(), b" -> ",out.stdout.as_slice()].concat().as_slice()).await;
                             // remove folder, create new local from remote repo
-
+    
                         } else {
                             let data = result.unwrap().as_bytes();
                             write_logs([data, b" -> ",out.stdout.as_slice()].concat().as_slice()).await;
@@ -147,7 +156,7 @@ async fn main() {
                 }
             }
         }));
-
+    
     }
     let mut outputs = Vec::with_capacity(tasks.len());
     for task in tasks {
